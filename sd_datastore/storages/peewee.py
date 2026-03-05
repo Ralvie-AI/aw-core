cc09933231bbbafd46965665703bbbb9235b4267
hello0zai <hello0zaizai@gmail.com>
2026-03-05 16:37:59 +0700

    feat: add ocr_text field to screenshotmodel table

--- sd_datastore/storages/peewee.py
+++ sd_datastore/storages/peewee.py
@@ -112,6 +112,17 @@ def auto_migrate(db: Any, path: str) -> None:
         with db.atomic():
             migrate(migrator.add_column("eventmodel", "server_sync_status", server_sync_status_field))
 
+
+    # check if screenshotmodel has ocr_text field
+    info = db.execute_sql("PRAGMA table_info(screenshotmodel)")
+    has_ocr_text = any(row[1] == "ocr_text" for row in info)
+
+    # Add the ocr_text column to the screenshotmodel.
+    if not has_ocr_text:
+        ocr_text_field = TextField(null=True)
+        with db.atomic():
+            migrate(migrator.add_column("screenshotmodel", "ocr_text", ocr_text_field))
+
     db.close()
 
 
