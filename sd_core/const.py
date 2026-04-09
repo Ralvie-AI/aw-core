403c87812d3f5be631680724df9b18233d8ebcd4
hello0zai <hello0zaizai@gmail.com>
2026-04-09 12:39:41 +0700

    remove duplicate functions and unused code

--- sd_core/const.py
+++ sd_core/const.py
@@ -1,3 +1,5 @@
+import os 
+
 # DEVELOPMENT_MODE = 0 is for local development.
 # DEVELOPMENT_MODE = 1 is for production.
 
@@ -6,3 +8,5 @@ STAGING = 1
 INTERNAL = 0
 LOGGING_VERBOSE = 0
 CACHE_KEY = "Sundial"
+
+PUBLIC_KEY = os.path.join(os.environ['LOCALAPPDATA'], 'Sundial', 'Sundial', 'sd-server', '{email}-{company_id}-public.pem')
