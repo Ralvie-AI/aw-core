48db25291f4100ac23c64de055cdfd30e49fec00
hello0zai <hello0zaizai@gmail.com>
2026-06-09 10:59:14 +0700

    refactor: move constants to const file

--- sd_core/const.py
+++ sd_core/const.py
@@ -17,9 +17,12 @@ STATUS_SYNC_TIME = 180 # 3 minutes
 STATUS_SYNC_FIRST_TIME = 30 # 3 seconds
 
 
+MAX_RETRIES = 3
+DELAY_SECONDS = 3  # wait before retry
 
 # 1 set up for configurable server setting
 # 0 not set up for configurable server setting
 CONFIG_SERVER = 0
 
 PUBLIC_KEY = os.path.join(os.environ['LOCALAPPDATA'], 'Sundial', 'Sundial', 'sd-server', '{email}-{company_id}-public.pem')
+HOST_TO_UPLOAD_SHOT_GET = "{protocol}://{host}/web/events/screenshot?fileFormat=json&userId={user_id}&companyId={company_id}"
